f95686ade92691b22d897d9cd3116c2179f5dd27
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-08 05:55:50 +0300

    commit tree 34ec4fbe03ef13e22f2c86f74d2e329a6b001bba

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,25 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: {error: 'Maximum pageview limit reached'}, status: :unauthorized
+    end  
   end
 
   private
-
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
   end
-
 end
